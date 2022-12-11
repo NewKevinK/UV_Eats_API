c24f66cbf5e327ea7e4080fd2a6e488865bd4826
NewKevinK <zS19014047@estudiantes.uv.mx>
2022-12-11 00:28:31 -0600

    Solo test2

--- src/routes/test.js
+++ src/routes/test.js
@@ -2,11 +2,12 @@
 import express from 'express'
 const routes = express.Router()
 //const {validateCreate} = require('../Validators/testV.js')
+import { validateToken } from '../Helpers/jwtHelper.js';
 import {methods as test} from "../Controllers/test.js";
 
 
 routes.post("/",  test.addTest);
-routes.get("/", test.getTests);
+routes.get("/", validateToken, test.getTests);
 routes.get("/:id",test.getTest);
 
 
